3a06747cc6dc34833eb9bb5a7ae52e6b382b9c52
nivetha-005 <nivethavijayakumar005@gmail.com>
2025-07-04 21:58:01 +0530

    Add files via upload
    A program to print numbers from 1 to 10 in a single row with one tab space.

---
+++ PrintNum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class PrintNum{
+    public static void main(String args[]){
+        int n=10;
+        for(int i=1;i<=n;i++){
+            System.out.print(i +"\t");
+        }
+    }
+}
