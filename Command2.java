597fd6afcf04ae3ae215a6c43ee908ba4f0bc95e
nivetha-005 <nivethavijayakumar005@gmail.com>
2025-07-04 21:50:19 +0530

    Add files via upload
    A Program that accepts two Strings as command line arguments and generate 
    the output as ABC Technologies mumbai

---
+++ Command2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.Scanner;
+public class Command2{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String[] a=new String[3];
+    System.out.println("Enter the company name:");
+    a[0]=sc.next();
+    System.out.println("Enter the place:");
+    a[2]=sc.next();
+    System.out.println(a[0] + " Technologies " + a[2]);
+    }
+}
