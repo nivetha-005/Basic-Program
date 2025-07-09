5257ac1079f858a2f89531cf6116513836eff980
nivetha-005 <nivethavijayakumar005@gmail.com>
2025-07-09 18:34:07 +0530

    Add files via upload
    A Program to accept two integers as command line arguments and print the sum 
    of the two numbers

---
+++ TwoInteger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.util.Scanner;
+public class TwoInteger{
+    public static void main(String args[]){
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        System.out.println("The sum of a and b is:" +(a+b));
+    }
+}
